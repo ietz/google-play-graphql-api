fbd6be6a7e79a229cdab1fecb92ebee5869b34d0
Tim Pietz <tim@pietz.me>
2019-10-30 16:42:09 +0100

    Refactor: Extract everything related to lazy loading

--- src/lazy.js
+++ src/lazy.js
@@ -1,25 +1,37 @@
-export const lazy = (obj, properties, loadProperty) => {
-	return new Proxy(obj, new LazyObjectProxyHandler(properties, loadProperty));
-};
+export class LazyLoader {
+	/**
+	 * Describes an object which is to be loaded lazily
+	 *
+	 * @param properties List of properties which can be made available when requested
+	 * @param load Called when a new property is requested.
+	 * Receives the object and the property name as a parameter and should return the retrieved
+	 * properties as an object.
+	 */
 
-class LazyObjectProxyHandler {
-	constructor(properties, loadProperty) {
+	constructor(properties, keyExtractor, load) {
 		this.properties = properties;
-		this.loadProperty = loadProperty;
-		this.requests = {};
+		this.load = load;
+		this.keyExtractor = keyExtractor;
+	}
+}
+
+export const withLazyParent = (loader, resolvers) =>
+	new Proxy(resolvers, new LazyParentResolverProxyHandler(loader));
+
+
+
+class AugmentingProxyHandler {
+	/**
+	 * A proxy handler which augments the target object with a predefined list of properties
+	 *
+	 * @param properties the properties to be added to the object
+	 */
+
+	constructor(properties) {
+		this.properties = properties;
 	}
 
-	get = async (target, property) => {
-		if (!(property in target) && this.properties.includes(property)) {
-			if (!(target in this.requests)) {
-				this.requests[target] = Promise.resolve(this.loadProperty(property)).then((data) => {
-					Object.assign(target, data);
-				});
-			}
-
-			await this.requests[target];
-		}
-
+	get = (target, property) => {
 		return target[property];
 	};
 
@@ -33,4 +45,71 @@ class LazyObjectProxyHandler {
 			...(Reflect.ownKeys(target) || []),
 		]));
 	};
+
+	getOwnPropertyDescriptor = (target, property) => {
+		if (property in target) {
+			return Object.getOwnPropertyDescriptor(target, property);
+		} else {
+			return {
+				value: this.get(target, property),
+				writable: false,
+				configurable: true,
+				enumerable: true,
+			}
+		}
+	};
 }
+
+
+class LazyObjectProxyHandler extends AugmentingProxyHandler {
+	/**
+	 * A proxy handler which can be used to fetch object properties only when they are requested (lazily)
+	 * e.g. if only a subset of the object properties are available in cache, the remaining properties
+	 * will be retrieved as soon as they are requested.
+	 *
+	 * @param loader LazyLoader describing the object
+	 */
+
+	constructor(loader) {
+		super(loader.properties);
+		this.loader = loader;
+		this.requests = {};
+	}
+
+	get = async (target, property) => {
+		if (!(property in target) && this.loader.properties.includes(property)) {
+			const targetKey = this.loader.keyExtractor(target);
+
+			if (!(target in this.requests)) {
+				this.requests[targetKey] = Promise.resolve(this.loader.load(targetKey, property)).then((data) => {
+					Object.assign(target, data);
+				});
+			}
+
+			await this.requests[targetKey];
+		}
+
+		return target[property];
+	};
+}
+
+
+class LazyParentResolverProxyHandler extends AugmentingProxyHandler {
+	constructor(loader) {
+		super(loader.properties);
+		this.lazyParentProxyHandler = new LazyObjectProxyHandler(loader);
+	};
+
+	get = (target, property) => {
+		return (parent, args, context, info) => {
+			const lazyParent = new Proxy(parent, this.lazyParentProxyHandler);
+
+			if (property in target) {
+				return target[property](lazyParent, args, context, info);
+			} else {
+				return lazyParent[property];
+			}
+		};
+	};
+}
+
--- src/server.js
+++ src/server.js
@@ -3,12 +3,17 @@ import {buildSchema} from 'graphql';
 import { GraphQLServer } from "graphql-yoga";
 import gplay, { category, collection, sort, age, permission } from 'google-play-scraper';
 import {importSchema} from "graphql-import";
-import {lazy} from "./lazy";
+import {LazyLoader, withLazyParent} from "./lazy";
 
 const port = 4000;
 
 const typeDefs = importSchema('./dist/schema.graphql');
 const appFields = Object.keys(buildSchema(typeDefs).getType('App').getFields());
+const appLoader = new LazyLoader(
+	appFields,
+	app => app.appId,
+	id => gplay.app({appId: id})
+);
 
 const defaultPlaygroundQuery =
 `{
@@ -19,40 +24,6 @@ const defaultPlaygroundQuery =
 }
 `;
 
-const appResolverHandler = {
-	get: (target, key) => {
-		return (parent, args, context, info) => {
-			const lazyParent = lazy(parent, appFields, () => gplay.app({appId: parent.appId}));
-
-			if (key in target) {
-				return target[key](lazyParent, args, context, info);
-			} else {
-				return lazyParent[key];
-			}
-		};
-	},
-	has: (target, key) => {
-		return key in target || appFields.includes(key);
-	},
-	ownKeys: (target) => {
-		return Array.from(new Set([
-			...appFields,
-			...(Reflect.ownKeys(target) || []),
-		]));
-	},
-	getOwnPropertyDescriptor: (target, key) => {
-		if (key in target) {
-			return Object.getOwnPropertyDescriptor(target, key);
-		} else {
-			return {
-				value: appResolverHandler.get(target, key),
-				writable: false,
-				configurable: true,
-				enumerable: true,
-			}
-		}
-	},
-};
 
 const mapOpts = (opts) => {
 	return Object.entries({ category, collection, sort, age, permission })
@@ -77,12 +48,12 @@ const resolvers = {
 			return await gplay.list(mapOpts(params));
 		},
 	},
-	App: new Proxy({
+	App: withLazyParent(appLoader, {
 		histogram: async (parent) => {
 			const hist = await parent.histogram;
 			return [1, 2, 3, 4, 5].map(stars => hist[String(stars)] || 0)
 		},
-	}, appResolverHandler),
+	}),
 };
 
 const server = new GraphQLServer({ typeDefs, resolvers });
